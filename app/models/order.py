c1862755292ae1268fcf48a2d57732f4b455a4ed
Nan <nan_guo@hotmail.com>
2023-01-26 15:45:07 -0800

    order feature

---
+++ app/models/order.py
@@ -0,0 +1,14 @@
+from .db import db, SCHEMA, environment, add_prefix_for_prod
+import datetime
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,
+        db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
