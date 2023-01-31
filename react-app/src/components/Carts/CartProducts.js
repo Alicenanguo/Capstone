c0b77b66e1ff4699423f4e8fd5665bd6462d613b
Nan <nan_guo@hotmail.com>
2023-01-30 17:04:48 -0800

    cart update

--- react-app/src/components/Carts/CartProducts.js
+++ react-app/src/components/Carts/CartProducts.js
@@ -1,93 +1,101 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink, Route, useParams, useHistory  } from "react-router-dom";
-import { updateCartThunk, deleteCartThunk,resetCartThunk} from "../../store/cart";
+import { NavLink, Route, useParams, useHistory } from "react-router-dom";
+import {
+  getCartProductsThunk,
+  updateCartThunk,
+  deleteCartThunk,
+  resetCartThunk,
+} from "../../store/cart";
 import "./cart.css";
 
 const CartProducts = ({ product }) => {
-    const history = useHistory()
-    const dispatch = useDispatch();
-    const [quantity, setQuantity] = useState(product?.quantity)
-    const [productNum, setProductNum] = useState(product?.Product?.quantity)
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [quantity, setQuantity] = useState(product?.quantity);
+  const [productNum, setProductNum] = useState(product?.Product?.quantity);
     const [errors, setErrors] = useState([]);
 
-    let carts = useSelector(state => Object.values(state.carts.Cart))
+    useEffect(() => {
+        dispatch(getCartProductsThunk());
+    }, [dispatch]);
 
-    const updateCartNum = async (e) => {
-        e.preventDefault();
+    let carts = useSelector((state) => Object.values(state.carts.Cart));
+    if (!carts) return null;
 
-        const update = await dispatch(updateCartThunk(product.id, { quantity }))
-            .catch(async (res) => {
-                const result = await res.json();
-                if (result && result.errors) {
-                    setErrors(result.errors)
-                }
-            })
+    let finalPrice = carts.reduce((final,cart) => final + cart.Product.price * cart.quantity,0)
 
-        if (update) {
-            setErrors([])
-        }
-    }
-    //     let countPrice = (carts) => {
-    //         let price = 0;
-    //         for (let i = 0; i < carts.length; i++) {
-    //             price += Number(carts[i].quantity * carts[i].Product.price)
-    //         }
-    //         return price.toFixed(2)
-    //     }
-    //     console.log('count-price',countPrice(carts))
+//   const updateCartNum = async (e) => {
+//     e.preventDefault();
 
+//     const update = await dispatch(
+//       updateCartThunk(product.id, { quantity })
+//     ).catch(async (res) => {
+//       const result = await res.json();
+//       if (result && result.errors) {
+//         setErrors(result.errors);
+//       }
+//     });
 
-    // let checkout =() => {
-    //     dispatch(resetCartThunk())
-    //     history.push('/carts/checkout')
-    // }
+//     if (update) {
+//       setErrors([]);
+//     }
+//   };
 
-
-    return (
+  return (
+    <div>
+      <div>
+        <NavLink to={`/products/${product?.product_id}`}>
+          <img
+            className="product_img_all"
+            src={product?.Product?.preview_image}
+            alt={product?.Product?.name}
+            onError={(e) => {
+              e.currentTarget.src = "/default.jpeg";
+            }}
+          />
+          <div>{product?.Product?.name}</div>
+        </NavLink>
+        <div className="text-free-shipping">Eligible for Free Shipping!</div>
         <div>
-            <div>
-                <NavLink to={`/products/${product?.product_id}`}>
-                <img
-                      className="product_img_all"
-                      src={product?.Product?.preview_image}
-                      alt={product?.Product?.name}
-                      onError={e => { e.currentTarget.src = "/default.jpeg" }}
-
-                    />
-                    <div>{product?.Product?.name }</div>
-                </NavLink>
-                <div className='text-free-shipping'>Eligible for Free Shipping!</div>
-                <div>
-                    <form onSubmit={updateCartNum}>
-                        <ul className="error-container">
-                        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                        </ul>
-                        <div className="quantity-input">
-                            <input
-                            id='number'
-                            type="number"
-                            onChange={(e) => setQuantity(e.target.value)}
-                            value={quantity}
-                            min="1"
-                            max={productNum}
-                            step='1'
-                                required
-                            />
-                        </div>
-                    </form>
-                    <div className='delete-cart'>
-                        <button onClick={() => dispatch(deleteCartThunk(product?.id))}>
-                            Remove
-                        </button>
-                    </div>
-                </div>
-                <div className='cart-one-right-price'>${`${(quantity * (product?.Product?.price)).toFixed(2)}`}</div>
+                  <form
+                    //   onSubmit={updateCartNum}
+                  >
+            <ul className="error-container">
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+            <div className="quantity-input">
+              <select
+                id="number"
+                type="number"
+                onChange={(e) => setQuantity(e.target.value)}
+                value={quantity}
+                min="1"
+                max={productNum}
+              >
+                {[...Array(11).keys()].slice(1).map((num) => (
+                  <option key={num} value={num}>
+                    {num}
+                  </option>
+                ))}
+              </select>
             </div>
-
-
+          </form>
+          <div className="delete-cart">
+            <button onClick={() => dispatch(deleteCartThunk(product?.id))}>
+              Remove
+            </button>
+          </div>
         </div>
-    )
-}
+              <div className="cart-one-right-price">
+                  <span>Item(s) total</span>
+                  <span>${finalPrice?.toFixed(2) }</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CartProducts;
--- react-app/src/components/Carts/GetCarts.js
+++ react-app/src/components/Carts/GetCarts.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, Route, useParams, useHistory } from "react-router-dom";
-import CartProducts from "./CartProducts";
+// import CartProducts from "./CartProducts";
+import UpdateCart from "./UpdateCart";
 import {
   getCartProductsThunk,
   deleteCartThunk,
@@ -13,37 +14,33 @@ import "./cart.css";
 
 const GetCart = () => {
   const dispatch = useDispatch();
-    const history = useHistory();
+  const history = useHistory();
+  const [errors, setErrors] = useState([]);
 
   let carts = useSelector((state) => Object.values(state.carts.Cart));
   console.log("cart_in_getCart", carts);
+  let finalPrice = carts.reduce((final, cart) => final + cart?.Product?.price * cart?.quantity, 0)
+  console.log("finalPrice-in-car-checkout",finalPrice)
 
   useEffect(() => {
-      dispatch(getCartProductsThunk());
+    dispatch(getCartProductsThunk());
     //   dispatch(updateCartThunk(product.id, { quantity }))
   }, [dispatch]);
 
   if (!carts) {
     return null;
   }
-  let countProduct = (carts) => {
-    let count = 0;
-    for (let i = 0; i < carts.length; i++) {
-      count += Number(carts[i].quantity);
-    }
-    return count;
-  };
 
-  let countPrice = (carts) => {
-    let price = 0;
-      for (let i = 0; i < carts.length; i++) {
-        let product=carts[i]
-          price += product.quantity * product?.Product?.price;
-          console.log('quantitiy',product.quantity)
-    }
-    return price.toFixed(2);
-  };
-  console.log("count-price", countPrice(carts));
+  // let countPrice = (carts) => {
+  //   let price = 0;
+  //     for (let i = 0; i < carts.length; i++) {
+  //       let product=carts[i]
+  //         price += product.quantity * product?.Product?.price;
+  //         console.log('quantitiy',product.quantity)
+  //   }
+  //   return price.toFixed(2);
+  // };
+  // console.log("count-price", countPrice(carts));
 
   let checkout = () => {
     dispatch(resetCartThunk());
@@ -51,65 +48,74 @@ const GetCart = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>
-          <h1 className="text_shopping_cart">Shopping Cart</h1>
-          <div>
-            <i class="fa-solid fa-umbrella"></i>
-            <span>
-              Nansty Purchase Protection: Shop confidently on Nansty knowing if
-              something goes wrong with an order, we've got your back.
-            </span>
-          </div>
+    <>
+      <h1 className="text_shopping_cart">Shopping Cart</h1>
+      <i class="fa-solid fa-umbrella"></i>
+      <span>
+        Nansty Purchase Protection: Shop confidently on Nansty knowing if
+        something goes wrong with an order, we've got your back.
+      </span>
 
-                  <div className="cart-left">
+      <div className="cart-left">
+        {carts.length > 0 ? (
+          carts.map(el =>
+          (
+            <>
+              <NavLink to={`/products/${el?.product_id}`}>
+                <img
+                  className="product_img_all"
+                  src={el?.Product?.preview_image}
+                  alt={el?.Product?.name}
+                  onError={(e) => {
+                    e.currentTarget.src = "/default.jpeg";
+                  }}
+                />
+                <div>{el?.Product?.name}</div>
+              </NavLink>
+              <div className="text-free-shipping">Eligible for Free Shipping!</div>
+              <ul className="error-container">
+                {errors.map((error, idx) => (
+                  <li key={idx}>{error}</li>
+                ))}
+              </ul>
 
-                      {carts.length > 0 ?
-
-                          (
-
-                          carts.map((el) => (
-                                          <CartProducts key={el?.id} product={el} />
-                                          ))
-
-
-
-
-
-
-                              )
-
-
-                              : (
-              <div className="cart-one-middle-title">
-                                  Shopping cart is empty.
-                                  <div>
-                                      <NavLink to={`/`}>
-                                      Discover something unique to fill it up!
-                                      </NavLink>
-                                  </div>
-              </div>
-            )}
-            {/* <div className="cart-right">
               <div>
-                <div> Item(s) total </div>
-                <div>${countPrice(carts)}</div>
+                <UpdateCart cart={el} />
               </div>
-              <div>
-                {carts.length && (
-                  <div>
-                    <button onClick={checkout}>Proceed to checkout</button>
-                  </div>
-                                  )}
-
+              <div className="delete-cart">
+                <button onClick={() => dispatch(deleteCartThunk(el?.id))}>
+                  Remove
+                </button>
               </div>
-            </div> */}
+          <div className="cart-right">
+          <div className="checkout-box">
+            <div className="text-pay">You will pay:</div>
+            <span className="text-final-price">Item(s) total:</span>
+            <sapn>${finalPrice.toFixed(2)}</sapn>
           </div>
-        </div>
+            </div>
+            </>
+          ))
+
+
+
+        ) :
+          (
+            <div className="cart-one-middle-title">
+              Shopping cart is empty.
+              <div>
+                <NavLink to={`/`}>
+                  Discover something unique to fill it up!
+                </NavLink>
+              </div>
+            </div>
+          )
+
+        }
       </div>
-    </div>
+    </>
+
   );
-};
+}
 
 export default GetCart;
