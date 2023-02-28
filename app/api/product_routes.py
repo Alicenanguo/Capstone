a7fba24bc24c53953560f9913356e36f845a3b1b
Nan <nan_guo@hotmail.com>
2023-02-27 16:52:54 -0800

    AWS

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import db, Product, ProductImage,Review,Cart
 from app.forms import ProductForm,ReviewForm,CartForm
+from app.s3_helpers import (
+    upload_file_to_s3, allowed_file, get_unique_filename)
 
 product_routes = Blueprint('products', __name__)
 
@@ -183,3 +185,33 @@ def search_product(keyword):
       product.to_dict_detail() for product in products
     ]
   }
+
+
+#AWS upload images
+@product_routes.route("/uploadImage", methods=["POST"])
+@login_required
+def upload_image():
+    if "image" not in request.files:
+        return {"errors": "image required"}, 400
+
+    image = request.files["image"]
+
+    if not allowed_file(image.filename):
+        return {"errors": "file type not permitted"}, 400
+
+    image.filename = get_unique_filename(image.filename)
+
+    upload = upload_file_to_s3(image)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+    # flask_login allows us to get the current user from the request
+    # new_image = Image(user=current_user, url=url)
+    # db.session.add(new_image)
+    # db.session.commit()
+    return {"url": url}
